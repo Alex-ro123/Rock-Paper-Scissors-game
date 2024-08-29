440e27693cd45d7cef43ff960322c670f0cce64c
Alex-ro123 <145465359+Alex-ro123@users.noreply.github.com>
2024-08-29 22:28:53 +0300

    function getHumanChoise created
    user can chose between rock, paper, scissors

--- javascript.js
+++ javascript.js
@@ -1,14 +1,38 @@
 function getComputerChoise() {
-  let choise = Math.floor(Math.random() * 3);
+  const choise = Math.floor(Math.random() * 3);
   switch (choise) {
-    case 0:
+    case 1:
       return "rock";
 
-    case 1:
-      return "paper";
     case 2:
+      return "paper";
+    case 3:
       return "scissors";
-    default:
-      return "What an unexpected number";
   }
 }
+
+function getHumanChoise() {
+  const choise = prompt("Chose rock, paper or scissors");
+
+  if (choise === "rock") {
+    return "rock";
+  } else if (choise === "paper") {
+    return "paper";
+  } else if (choise === "scissors") {
+    return "scissors";
+  } else {
+    return "What an unexpected number";
+  }
+}
+
+let humanScore = 0;
+let computerScore = 0;
+
+function playRound(humanChoice, computerChoice) {
+  // your code here!
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
