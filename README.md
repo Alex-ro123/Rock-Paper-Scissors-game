64d58844f098e9911c99d6a21bd9ba62510c6972
Alex-ro123 <145465359+Alex-ro123@users.noreply.github.com>
2024-08-30 12:29:52 +0300

    Created the first function geComputerChoice

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # Rock-Paper-Scissors-game
+
+1. create function getComputerChoice
+
+The computer must chose between 3 values rock, paper, scissors. For this we need to apply the Math.Random () method who normally returns a number between 0 and 1, but now the computer must chose between rock, paper and scissors so we have to use also the Math.floor method to increase the number range to 3.
+Now we have to find a way to give the computer the 3 values to chose from.
+
+2. create function getHumanChoice
+
+For this we need to create a variable with the value prompt method to display the options to choose from.
+There are 3 possible values to chose from and a forth one in case of player writes something else.
+
+3. Create two variables for score in the global scope: humanScore and computerScore with the initial value 0.
+
+4. Write the logic to play one round
+
+In this section we must declare all possible outcomes of the game:
+rock>scissors
+paper>rock
+scissors>paper
+Also in the return statement we need to include the outcome
--- javascript.js
+++ javascript.js
@@ -1,38 +1,11 @@
-function getComputerChoise() {
-  const choise = Math.floor(Math.random() * 3);
-  switch (choise) {
-    case 1:
-      return "rock";
-
-    case 2:
-      return "paper";
-    case 3:
-      return "scissors";
+function getComputerChoice () {
+  let choice = Math.floor(Math.random()*3);
+  
+  if (choice==='rock') {
+    return 'rock';
+  } else if (choice==='paper') {
+    return 'paper';
+  } else (choice ==='scissors') {
+    return 'scissors';
   }
 }
-
-function getHumanChoise() {
-  const choise = prompt("Chose rock, paper or scissors");
-
-  if (choise === "rock") {
-    return "rock";
-  } else if (choise === "paper") {
-    return "paper";
-  } else if (choise === "scissors") {
-    return "scissors";
-  } else {
-    return "What an unexpected number";
-  }
-}
-
-let humanScore = 0;
-let computerScore = 0;
-
-function playRound(humanChoice, computerChoice) {
-  // your code here!
-}
-
-const humanSelection = getHumanChoice();
-const computerSelection = getComputerChoice();
-
-playRound(humanSelection, computerSelection);
